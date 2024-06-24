5cf9601e2776de86ab16b818cbf4873329220cbb
Vennela potla <144135501+Vennip@users.noreply.github.com>
2024-06-24 17:51:09 +0530

    modified

--- src/App.js
+++ src/App.js
@@ -1,26 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
-import Login  from './Components/Login/Login';
-import Verifymail from './Components/Verifymail';
-import Transactions from './Components/Transactions';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Recharge from './Components/Recharge';
-import Fundtransfer from './Components/Fundtransfer';
+import React from 'react'
 
-
-function App() {
+export const App = () => {
   return (
-    <div className="App">
-     <BrowserRouter>
-     <Routes>
-     
-  <Route path='/' element={<Fundtransfer/>}  />
-
-   
-     </Routes>
-     </BrowserRouter>
-    </div>
-  );
+    <div>App</div>
+  )
 }
-   
-export default App;
