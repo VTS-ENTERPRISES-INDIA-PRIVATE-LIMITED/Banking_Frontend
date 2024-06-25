0e3b9e16c763d0d9f742758329a8ab7bdaccfacb
Naga sai subba reddy <121428764+20A21A04D2@users.noreply.github.com>
2024-06-25 12:11:46 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+<<<<<<< Updated upstream
+=======
+<<<<<<< HEAD
+import { Login } from './Components/Login/Login';
+import Transactions from './Components/Transactions/Transactions'
+=======
+>>>>>>> Stashed changes
 
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import Login from './Components/Login/Login'
@@ -7,10 +14,24 @@ import ForgotPassword from './Components/ForgotPassword/ForgotPassword'
 import ResetPassword from './Components/PasswordReset/PasswordReset';
 import React, { useState } from 'react';
 import Tabss from './Components/Tabss'
+<<<<<<< Updated upstream
+=======
+>>>>>>> b616d38836350858cba136b654830ceb3808a9db
+>>>>>>> Stashed changes
 
 function App() {
   
   return (
+<<<<<<< Updated upstream
+=======
+<<<<<<< HEAD
+    <div className="App">
+     <Login />
+     <Transactions />
+    </div>
+  );
+=======
+>>>>>>> Stashed changes
     
       <BrowserRouter>
         <Routes>
@@ -24,5 +45,9 @@ function App() {
       
       </BrowserRouter>
   )
+<<<<<<< Updated upstream
+=======
+>>>>>>> b616d38836350858cba136b654830ceb3808a9db
+>>>>>>> Stashed changes
 }
 export default App;
