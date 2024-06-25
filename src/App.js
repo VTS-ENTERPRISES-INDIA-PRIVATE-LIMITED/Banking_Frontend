410a1c6dde8f50e84acce326ff70bfed7bc004a4
chinnasivakrishna <139040548+chinnasivakrishna@users.noreply.github.com>
2024-06-25 12:12:06 +0530

    logincss

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import logo from './logo.svg';
 import './App.css';
-<<<<<<< HEAD
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import Login from './Components/Login/Login'
 import ForgotPassword from './Components/ForgotPassword/ForgotPassword'
@@ -12,26 +11,18 @@ function App() {
   
   return (
     
-      <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<Tabss />} />
-          <Route path='/login' element={<Login />} />
-          <Route path='/ForgotPassword' element={<ForgotPassword />} />
-          <Route path='/ResetPassword' element={<ResetPassword />} />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Tabss />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/ForgotPassword' element={<ForgotPassword />} />
+        <Route path='/ResetPassword' element={<ResetPassword />} />
 
           
-        </Routes>
+      </Routes>
       
-      </BrowserRouter>
+    </BrowserRouter>
       
-=======
-import { Login } from './Components/Login/Login';
-
-function App() {
-  return (
-    <div className="App">
-     <Login />
-    </div>
->>>>>>> d100549b473039f134e742ef7d0d2ff560f63cfe
-  );
+  )
 }
+ export default App
